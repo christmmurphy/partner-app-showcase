ee3413ad2340eb65bf36fc11c5de3f5b6f892849
Chris Murphy <chrismurphy@Chriss-MBP-2.attlocal.net>
2022-05-27 17:29:02 -0500

    Initial Comit, bones+readme

---
+++ README.md
@@ -0,0 +1,7 @@
+## Configure
+* Clone Repo To Desktop
+* Open Terminal, navigate to repo
+* `ruby app.rb`
+* Download [Ngrok](https://ngrok.com/) for local testing
+* navigate to ngrok and run `ngrok http 4567` to expose local ruby server
+* Point Intercom app package to <ngrok-generated-endpoint>/submit, /config etc
---
+++ app.rb
@@ -0,0 +1,25 @@
+require "json"
+require "sinatra"
+require "intercom"
+
+# Useful tools for manipulating JSON
+## https://codebeautify.org/jsonminifier
+## https://codebeautify.org/javascript-escape-unescape
+
+# Create an Intercom client to make REST API calls
+access_token = ENV['TOKEN']
+intercom = Intercom::Client.new(token: access_token)
+
+post'/' do # This section listens for the initial post from Intercom and retruns the default state of the card when the app is first loaded by the messenger
+  aircallCard = File.read('cards/aircall/main.json')
+  aircallCard # Return the card
+end
+
+post '/submit' do # When a user presses a button in your app, return a card
+  aircallCalling = File.read('cards/aircall/calling.json')
+  aircallCalling # Return the card
+end
+
+post'/configure' do
+  # To Do | Build a configure card to allow admins to select WHICH card is inserted.
+end
